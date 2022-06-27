7437dd3a5e7f025766ef464b78cbb1551236ae3c
Jelte Dirks <j.dirks@outlook.com>
2022-06-28 00:29:18 +0200

    add clap and set some cli args
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = CLIArgs::parse();
+    println!("{:?}", args);
+}
+
+#[derive(Parser, Debug)]
+struct CLIArgs {
+    #[clap(short = 'o', long = "output", parse(from_os_str))]
+    output: std::path::PathBuf,
+
+    #[clap(short = 'i', long = "input", parse(from_os_str))]
+    input: std::path::PathBuf,
 }
