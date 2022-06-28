a9e4b9020d86bc8f369db3a9954a14b2e25a2018
Jelte Dirks <j.dirks@outlook.com>
2022-06-28 21:22:11 +0200

    some annotations, probably add to this in other unrelated ocmmits as well
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args = CLIArgs::parse();
     let img = match ImageReader::open(args.input) {
         Ok(i) => i, // image could be opened
-        Err(e) => panic!("{}", e),
+        Err(e) => panic!("{}", e), // error while opening original file
     };
 
     println!("{:?}", img.into_dimensions());
@@ -14,6 +14,12 @@ fn main() {
 
 #[derive(Parser, Debug)]
 struct CLIArgs {
+    // needs some more information for doing the task
+    // some of the things that could be useful is
+    // 1. output directory location
+    // 2. the dimension package that is required?
+    // 3. which filter to choose from when resizing (advanced)
+
     #[clap(short = 'i', long = "input", parse(from_os_str))]
     input: std::path::PathBuf,
 }
