a1eeeda92110e93a9533315971fe2be6d192e961
Jelte Dirks <j.dirks@outlook.com>
2022-06-28 22:18:20 +0200

    clone the file and make two images, one for consuming to dimensions and one to keep
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use clap::Parser;
 use image::io::Reader as ImageReader;
-use std::env;
+use image::ImageFormat;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 fn main() {
     let args = CLIArgs::parse();
-    let img = match ImageReader::open(args.input) {
-        Ok(i) => i, // image could be opened
-        Err(e) => panic!("{}", e), // error while opening original file
-    };
+    let original = ImageRecord::new(args.input);
 
-    println!("{:?}", img.into_dimensions());
+    println!("we have dimensions: {:?}", original.dimensions);
 }
 
 #[derive(Parser, Debug)]
@@ -24,8 +24,52 @@ struct CLIArgs {
     input: std::path::PathBuf,
 }
 
-struct Original {
+struct ImageRecord {
+    path: PathBuf,
+    image: ImageReader<BufReader<File>>,
     dimensions: (u32, u32),
-    path: std::path::PathBuf,
 }
 
+impl ImageRecord {
+    pub fn new(p: PathBuf) -> ImageRecord {
+
+        // create a file from our input
+        let file = match File::open(&p) {
+            Ok(f) => {
+                println!("successfully opened the file {:?}", &p);
+                f
+            },
+            Err(e) => panic!("{}", e),
+        };
+        
+        // create a clone from the file, this uses the same file descriptor as
+        // the original, so operations technically affect both. we only need
+        // this for dimensions though...
+        let f_clone = match file.try_clone() {
+            Ok(f) => {
+                println!("successfully cloned the file");
+                f
+            }
+            Err(e) => panic!("{}", e),
+        };
+
+        // create an image to store in our record
+        let original = ImageReader::new(BufReader::new(file));
+        // create a clone so we can set the dimensions
+        let clone = ImageReader::new(BufReader::new(f_clone));
+        // try guessing the format, sets it automatically
+        let clone = clone.with_guessed_format().unwrap();
+
+        // set the dimensions of this image, consumes the image reader
+        let dimensions = match clone.into_dimensions() {
+            Ok(d) => d,
+            Err(e) => panic!("{}", e),
+        };
+
+        return ImageRecord {
+            path: p,
+            image: original,
+            dimensions
+        }
+    }
+}
