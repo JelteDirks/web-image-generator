f0c3371d8603516386e8d42e2a2d338ab0a3bcce
Jelte Dirks <j.dirks@outlook.com>
2022-06-28 23:21:29 +0200

    move image record to new file
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
-use image::io::Reader as ImageReader;
-use image::ImageFormat;
-use std::fs::File;
-use std::io::BufReader;
 use std::path::PathBuf;
+
+mod image_record;
+use image_record::ImageRecord;
 
 fn main() {
     let args = CLIArgs::parse();
@@ -22,62 +21,4 @@ struct CLIArgs {
 
     #[clap(short = 'i', long = "input", parse(from_os_str))]
     input: std::path::PathBuf,
-}
-
-struct ImageRecord {
-    path: PathBuf,
-    image: ImageReader<BufReader<File>>,
-    dimensions: (u32, u32),
-}
-
-impl ImageRecord {
-    pub fn new(p: PathBuf) -> ImageRecord {
-
-        // create a file from our input
-        let file = match File::open(&p) {
-            Ok(f) => f,
-            Err(e) => {
-                eprintln!("error opening file: {:?}", &p);
-                panic!("{}", e);
-            },
-        };
-
-        // create a clone from the file, this uses the same file descriptor as
-        // the original, so operations technically affect both. we only need
-        // this for dimensions though...
-        let f_clone = match file.try_clone() {
-            Ok(f) => {
-                println!("successfully cloned the file");
-                f
-            }
-            Err(e) => panic!("{}", e),
-        };
-
-        // create an image to store in our record
-        let original = ImageReader::new(BufReader::new(file));
-        // create a clone so we can set the dimensions
-        let clone = ImageReader::new(BufReader::new(f_clone));
-        // try guessing the format, sets it automatically
-        let clone = clone.with_guessed_format().unwrap();
-
-        // set the dimensions of this image, consumes the image reader
-        let dimensions = match clone.into_dimensions() {
-            Ok(d) => d,
-            Err(e) => panic!("{}", e),
-        };
-
-        return ImageRecord {
-            path: p,
-            image: original,
-            dimensions
-        };
-    }
-
-    pub fn get_dimensions(&self) -> &(u32, u32) {
-        return &self.dimensions;
-    }
-
-    pub fn get_path(&self) -> &PathBuf {
-        return &self.path;
-    }
 }
