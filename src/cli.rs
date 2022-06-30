190e132056ac4359bedf9ea2f5ae193757ffabe0
Jelte Dirks <j.dirks@outlook.com>
2022-06-30 21:21:19 +0200

    move clie to different file
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct CLIArgs {
+    // needs some more information for doing the task
+    // some of the things that could be useful is
+    // 1. output directory location
+    // 2. the dimension package that is required?
+    // 3. which filter to choose from when resizing (advanced)
+
+    #[clap(short = 'i', long = "input", parse(from_os_str))]
+    pub input: PathBuf,
+
+    #[clap(short = 'c', long = "config", parse(from_os_str))]
+    pub configuration: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
-use std::path::PathBuf;
 
 mod image_record;
 use image_record::ImageRecord;
+
 mod config;
 use config::{Config, SizeDescription};
+
+mod cli;
+use cli::CLIArgs;
 
 fn main() {
     let args = CLIArgs::parse();
@@ -15,17 +18,3 @@ fn main() {
 
 }
 
-#[derive(Parser)]
-struct CLIArgs {
-    // needs some more information for doing the task
-    // some of the things that could be useful is
-    // 1. output directory location
-    // 2. the dimension package that is required?
-    // 3. which filter to choose from when resizing (advanced)
-
-    #[clap(short = 'i', long = "input", parse(from_os_str))]
-    input: PathBuf,
-
-    #[clap(short = 'c', long = "config", parse(from_os_str))]
-    configuration: PathBuf,
-}
